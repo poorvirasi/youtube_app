9f27d8ae778acd2d3e4bd4ac2879a568d05334b4
Poorvigha <poorvirasi@gmail.com>
2019-07-31 06:35:56 +0530

    added post

--- django_app/views.py
+++ django_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django_app.models import Post
 # Create your views here.
 def home(request):
-    return render(request,'django_app/home.html')
+    context={
+    'posts':Post.objects.all()
+    }
+    return render(request,'django_app/home.html',context)
 
 
 def about(request):
